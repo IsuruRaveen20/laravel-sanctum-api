d184fd3961cdfe633450c60f7a0ea63d34748dd3
IsuruRaveen20 <isururaveen4520@gmail.com>
2023-07-20 22:19:25 +0530

    apis updated

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,22 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::post('/products', [ProductController::class, 'store']);
-// Route::get('/products/{product}', [ProductController::class, 'show']);
 
-Route::resource('products', ProductController::class);
-Route::get('/products/search/{name}',[ProductController::class, 'search']);
+// Route::resource('products', ProductController::class);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//Public Routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+
+//Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}',[ProductController::class, 'update']);
+    Route::delete('products/{id}', [ProductController::class, 'destroy']);
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
